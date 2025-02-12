6d269b9a40185705c8c76139845d0b42778ac36b
Digipr01 <tychokruithof2008@hotmail.com>
2025-02-12 20:59:28 +0100

    Finished mp4 converter

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Youtube Downloader
-not functional, check back soon
+Downloads mp4 of youtube video
+I wanted to make mp3 downloader, conversion has problems, mp4 it is
 
-### Made By
+## Disclaimer
+It is allowed to download copyrighted content for personal use. This means it is not allowed to re-upload this content to any platform without permission from the rightfull owner.
+
+### Project Made By
 Digipr01
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pytube as yt
-import pytube.exceptions as errors
+import pytubefix as yt
+import pytubefix.exceptions as errors
 import tkinter as tk
-
-youtubeObject = 0
+from tkinter import messagebox
 
 def Download(link):
+    youtubeObject = 0
     try:
         youtubeObject = yt.YouTube(link)
         youtubeObject = youtubeObject.streams.get_highest_resolution()
@@ -14,13 +14,33 @@ def Download(link):
         return 401, "Could not access video" #Unauthorised
     except:
         return 404, "Could not find video" #Video not found
+    youtubeObject.download('Downloads', filename=f"{youtubeObject.title}.mp4")
     try:
-        youtubeObject.download()
+        pass
     except:
         return 422, "Could not download video" #Uprocessable Content
-    print("Download is completed successfully")
+    return 200
 
+
+def onButtonPress(link):
+    responseCode = Download(link)
+    if responseCode == 200:
+        tk.messagebox.showinfo(title="Succes", message="Download is completed successfully")
+    else:
+        tk.messagebox.showerror(title="Failed", message=f"Response code: {responseCode[0]}\n{responseCode[1]} ")
+        
 root = tk.Tk()
-root.title="Oi"
+root.geometry('400x250')
+root.title("Youtube Downloader")
+
+textWindow = tk.Label(root, text="Insert yt link")
+textWindow.pack()
+
+linkInput = tk.Entry(root, relief=tk.SUNKEN, width=50)
+linkInput.pack()
+
+confirmButton = tk.Button(root, text="Download", command = lambda: onButtonPress(linkInput.get()))
+confirmButton.pack()
+root.bind('<Return>', lambda x: onButtonPress(linkInput.get())) #x holds the event variable
 
 root.mainloop()
